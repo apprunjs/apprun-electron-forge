e81ec2adc8df22a8c37dcbbe416f8271839bf7d3
Yiyi Sun <yiyisun@gmail.com>
2020-02-24 08:39:37 -0500

    add webpack plugin

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+import app from 'apprun';
+import { ipcRenderer } from 'electron';
+
+app.on('@electron', (...arg) => ipcRenderer.send('@electron', ...arg));
+ipcRenderer.on('@apprun', (_, event, ...args) => app.run(event, ...args));
+
+const view = () => (
+  <>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application using AppRun.</p>
+  </>
+);
+
+app.start(document.body, 0, view);
---
+++ src/main.ts
@@ -0,0 +1,88 @@
+import { app, BrowserWindow, Menu, ipcMain } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+
+declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    height: 800,
+    width: 1000,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {
+          label: 'Hello',
+          click() {
+            mainWindow.webContents.send('@apprun', '#hello', 'from Electron Main');
+          }
+        },
+        { type: 'separator' },
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'quit' }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it"s common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('@electron', (_, ...arg) => {
+  console.log(...arg);
+});
+
+app.allowRendererProcessReuse = true;
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
